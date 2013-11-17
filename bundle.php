27cdff712b470ce85321e0c13819d128ce120201
unknown <Michel>
2013-11-17 18:55:34 +1100

    fixes

--- bundle.php
+++ bundle.php
@@ -2,13 +2,9 @@
 namespace App\Intranet;
 
 class Bundle extends \Coxis\Core\BundleLoader {
-	public function load($queue) {
-		parent::load();
-	}
-
 	public function run() {
-		\Coxis\Admin\Libs\AdminMenu::$menu[0]['childs'][] = array('label' => __('Users'), 'link' => 'users');
-		\Coxis\Admin\Libs\AdminMenu::$home[] = array('img'=>\URL::to('intranet/icon.svg'), 'link'=>'users', 'title' => __('Users'), 'description' => __('Manage your users'));
+		\Coxis\Admin\Libs\AdminMenu::instance()->menu[0]['childs'][] = array('label' => __('Users'), 'link' => 'users');
+		\Coxis\Admin\Libs\AdminMenu::instance()->home[] = array('img'=>\URL::to('intranet/icon.svg'), 'link'=>'users', 'title' => __('Users'), 'description' => __('Manage your users'));
 		parent::run();
 	}
 }
