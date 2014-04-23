d41890d29a7315ede8ed96f5d50d84f92b230659
Michel Hognerud <leyou.m@gmail.com>
2014-04-23 18:01:09 +0800

    fixing asgard-install

--- asgard-install.php
+++ asgard-install.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__.'/../utils/FileManager.php';
+require_once __DIR__.'/../utils/FileManager.php';
 
 \Asgard\Utils\FileManager::copy(__DIR__.'/app/intranet', 'app/intranet');
 \Asgard\Utils\FileManager::copy(__DIR__.'/web/intranet', 'web/intranet');
