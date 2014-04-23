e1e35b353b9a49e23e89dd55ac2b1a43431a3124
Michel Hognerud <leyou.m@gmail.com>
2014-04-24 07:22:02 +0800

    fixes

--- asgard-install.php
+++ asgard-install.php
@@ -6,5 +6,5 @@ require _CORE_DIR_.'core.php';
 \Asgard\Utils\FileManager::copy(__DIR__.'/app/intranet', _DIR_.'app/intranet');
 \Asgard\Utils\FileManager::copy(__DIR__.'/web/intranet', _DIR_.'web/intranet');
 
-\Asgard\Orm\MigrationsManager::addMigrationFile(__DIR__.'/migrations/Intranet.php');
-\Asgard\Orm\MigrationsManager::migrate('Intranet');
+\Asgard\Orm\Libs\MigrationsManager::addMigrationFile(__DIR__.'/migrations/Intranet.php');
+\Asgard\Orm\Libs\MigrationsManager::migrate('Intranet');
